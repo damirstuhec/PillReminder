81c70ac3de169d3a7cb94cfcc12e483717fe066c
Damir Stuhec <damir.stuhec@gmail.com>
2012-04-05 21:24:30 +0200

    initial commit

---
+++ PillReminderProject/PillReminderProject/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  PillReminderProject
+//
+//  Created by Damir Stuhec on 4/5/12.
+//  Copyright (c) 2012 FERI Maribor, Slovenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PillReminderProject/PillReminderProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PillReminderProject
+//
+//  Created by Damir Stuhec on 4/5/12.
+//  Copyright (c) 2012 FERI Maribor, Slovenia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
