9e6caacdbe29525a3a4907b51879157dfc67739c
Damir Stuhec <damir.stuhec@gmail.com>
2012-04-27 17:57:43 +0200

    wheelPicker dokoncevanje

--- PillReminderProject/PillEditingViewController.m
+++ PillReminderProject/PillEditingViewController.m
@@ -11,13 +11,17 @@
 @interface PillEditingViewController ()
 @property (nonatomic, weak) IBOutlet UITextField *textField;
 @property (nonatomic, retain) NSArray *wheelPickerItemsArray;
-@property (weak, nonatomic) IBOutlet UIPickerView *wheelPicker;
+@property (nonatomic, weak) IBOutlet UIPickerView *wheelPicker;
+@property (nonatomic, readonly, getter=isEditingPillWeight) BOOL editingPillWeight;
 @end
 
 
 @implementation PillEditingViewController
+{
+    BOOL hasDeterminedWhetherEditingPillWeight;
+}
 
-@synthesize textField = _textField, editedPill=_editedPill, editedFieldKey=_editedFieldKey, editedFieldName=_editedFieldName, wheelPicker = _wheelPicker, wheelPickerItemsArray = _wheelPickerItemsArray;
+@synthesize textField = _textField, editedPill=_editedPill, editedFieldKey=_editedFieldKey, editedFieldName=_editedFieldName, wheelPicker = _wheelPicker, wheelPickerItemsArray = _wheelPickerItemsArray, editingPillWeight = _editingPillWeight;
 
 
 #pragma mark -
@@ -33,41 +37,33 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    NSMutableArray *temp = [[NSMutableArray alloc] initWithCapacity:1007];
-    [temp addObject:@"0.25"]; [temp addObject:@"0.50"]; [temp addObject:@"0.75"]; [temp addObject:@"1.0"]; 
-    [temp addObject:@"2.0"]; [temp addObject:@"3.0"]; [temp addObject:@"4.0"];
     
-    for (int i=5; i<1001; ) {
-        [temp addObject:[NSString stringWithFormat:@"%d.0", i]];
-        i+=5;
-    }
-    
-    self.wheelPickerItemsArray = [temp copy];
-    self.wheelPicker.hidden = NO;
-    self.textField.enabled = NO;
-    
-    // this view controller is the data source and delegate for wheelPicker
-	self.wheelPicker.delegate = self;
-	self.wheelPicker.dataSource = self;
-
     // Configure the user interface according to state.
-    /*if (self.editingDate) {
+    if (self.editingPillWeight) {
         
-        self.textField.hidden = YES;
-        self.datePicker.hidden = NO;
-        NSDate *date = [self.editedObject valueForKey:self.editedFieldKey];
-        if (date == nil) {
-            date = [NSDate date];
+        NSMutableArray *temp = [[NSMutableArray alloc] initWithCapacity:1007];
+        [temp addObject:@"0.25"]; [temp addObject:@"0.50"]; [temp addObject:@"0.75"]; [temp addObject:@"1.0"]; 
+        [temp addObject:@"2.0"]; [temp addObject:@"3.0"]; [temp addObject:@"4.0"];
+        
+        for (int i=5; i<1001; ) {
+            [temp addObject:[NSString stringWithFormat:@"%d.0", i]];
+            i+=5;
         }
-        self.datePicker.date = date;
-    }
-    else {
-    */    
+        
+        self.wheelPickerItemsArray = [temp copy];
+        self.wheelPicker.hidden = NO;
+        self.wheelPicker.delegate = self;
+        self.wheelPicker.dataSource = self;
+        self.textField.enabled = NO;
+    
+    }else {
+        self.wheelPicker.hidden = YES;
         self.textField.hidden = NO;
+        self.textField.enabled = YES;
         self.textField.text = [NSString stringWithFormat:@"%@", [self.editedPill valueForKey:self.editedFieldKey]];
         self.textField.placeholder = self.title;
-        //[self.textField becomeFirstResponder];
-    //}
+        [self.textField becomeFirstResponder];
+    }
 }
 
 #pragma mark -
@@ -79,13 +75,13 @@
     NSUndoManager * undoManager = [[self.editedPill managedObjectContext] undoManager];
     [undoManager setActionName:[NSString stringWithFormat:@"%@", self.editedFieldName]];
     
-    // Pass current value to the edited object, then pop.
-    //if (self.editingDate) {
-    //    [self.editedObject setValue:self.datePicker.date forKey:self.editedFieldKey];
-    //}
-    //else {
+    if (self.editingPillWeight) {
+        NSNumber *pillWeight = [NSNumber numberWithInt:[self.textField.text integerValue]];
+        [self.editedPill setValue:pillWeight forKey:self.editedFieldKey];
+    
+    }else {
         [self.editedPill setValue:self.textField.text forKey:self.editedFieldKey];
-    //}
+    }
     
     [self.navigationController popViewControllerAnimated:YES];
 }
@@ -102,13 +98,47 @@
     [super viewDidUnload];
 }
 
+#pragma mark -
+#pragma mark Manage whether editing a date
+
+- (void)setEditedFieldKey:(NSString *)editedFieldKey
+{
+    if (![_editedFieldKey isEqualToString:editedFieldKey]) {
+        hasDeterminedWhetherEditingPillWeight = NO;
+        _editedFieldKey = editedFieldKey;
+    }
+}
+
+
+- (BOOL)isEditingPillWeight
+{
+    if (hasDeterminedWhetherEditingPillWeight == YES) {
+        return _editingPillWeight;
+    }
+    
+    NSEntityDescription *entity = [self.editedPill entity];
+    NSAttributeDescription *attribute = [[entity attributesByName] objectForKey:self.editedFieldKey];
+    NSString *attributeClassName = [attribute attributeValueClassName];
+    
+    NSLog(@"%@", attributeClassName);
+    if ([attributeClassName isEqualToString:@"NSNumber"]) {
+        _editingPillWeight = YES;
+    }
+    else {
+        _editingPillWeight = NO;
+    }
+    
+    hasDeterminedWhetherEditingPillWeight = YES;
+    return _editingPillWeight;
+}
+
 
 #pragma mark -
 #pragma mark UIPickerViewDelegate
 
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
-    self.textField.text = [NSString stringWithFormat:@"%@ mg", [self.wheelPickerItemsArray objectAtIndex:[self.wheelPicker selectedRowInComponent:0]]];
+    self.textField.text = [NSString stringWithFormat:@"%@", [self.wheelPickerItemsArray objectAtIndex:[self.wheelPicker selectedRowInComponent:0]]];
 }
 
 
