1fc0c4349d49b92e3aea7e7e3da738527067cea3
Damir Stuhec <damir.stuhec@gmail.com>
2012-05-01 09:24:32 +0200

    formatiranje kode za boljši pregled nad potekom

--- PillReminderProject/PillDetailsViewController.m
+++ PillReminderProject/PillDetailsViewController.m
@@ -10,6 +10,7 @@
 #import "PillEditingViewController.h"
 #import "PillNotesList.h"
 
+
 @interface PillDetailsViewController()
 
 @property (nonatomic, strong) NSUndoManager *undoManager;
@@ -22,15 +23,20 @@
 
 @implementation PillDetailsViewController
 
+@synthesize pill = _pill;
+@synthesize undoManager = _undoManager;
+@synthesize pillNotes = _pillNotes;
+
 #define PILL_SECTION 0
 #define NOTES_SECTION 1
 #define REMINDER_SECTION 2
 
-@synthesize pill = _pill, undoManager = _undoManager, pillNotes = _pillNotes;
 
+#pragma mark -
+#pragma mark Getters and setters
 
-- (NSArray *)pillNotes {
-
+- (NSArray *)pillNotes
+{
     if (_pillNotes == nil) {
         
         NSString *warnings = @"";
@@ -47,11 +53,12 @@
         
         _pillNotes = [[NSArray alloc] initWithObjects:warnings, side_effects, storage, extra, nil];
     }
-    
     return _pillNotes;
 }
 
-- (void)setPillNotes:(NSArray *)pillNotes {
+
+- (void)setPillNotes:(NSArray *)pillNotes
+{
     _pillNotes = pillNotes;
 }
 
@@ -62,18 +69,18 @@
     self.navigationItem.rightBarButtonItem.enabled = [self.pill validateForUpdate:NULL];
 }   
 
-- (NSUInteger)calculateNumberOfNonNilProperties {
-    
+
+- (NSUInteger)calculateNumberOfNonNilProperties
+{    
     // Calculate number of required rows in pill section
     NSUInteger numberOfNonNilProperties = 4;
     
-    for (int i=0; i<4; i++)
-    {
+    for (int i=0; i<4; i++) {
         if (![self.pillNotes objectAtIndex:i] || [[self.pillNotes objectAtIndex:i] isEqualToString:@""]) numberOfNonNilProperties--;
     }
-    
     return numberOfNonNilProperties;
 }
+
 
 #pragma mark -
 #pragma mark View lifecycle
@@ -100,33 +107,29 @@
     [temp replaceObjectAtIndex:1 withObject:self.pill.side_effects];
     [temp replaceObjectAtIndex:2 withObject:self.pill.storage];
     [temp replaceObjectAtIndex:3 withObject:self.pill.extra];
-    
     self.pillNotes = [temp copy];
     
+	// Update pill on return.
     [self.tableView reloadData];
 }
 
 
-#pragma mark -
-#pragma mark Editing
-
-- (void)addPillNoteAddButton {
-    [self.tableView beginUpdates];
-    
-    NSUInteger numberOfNonNilProperties = [self calculateNumberOfNonNilProperties];
-    NSArray *pillPropertiesInsertIndexPath = [NSArray arrayWithObject:[NSIndexPath indexPathForRow:numberOfNonNilProperties inSection:NOTES_SECTION]];
-    
-    [self setUpUndoManager];
-    if (self.editing && numberOfNonNilProperties < 4) {
-        [self.tableView insertRowsAtIndexPaths:pillPropertiesInsertIndexPath withRowAnimation:UITableViewRowAnimationTop];    
-        
-    } else if (numberOfNonNilProperties < 4){
-        [self.tableView deleteRowsAtIndexPaths:pillPropertiesInsertIndexPath withRowAnimation:UITableViewRowAnimationTop];
-    }
-    
-    [self.tableView endUpdates];
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self becomeFirstResponder];
 }
 
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self resignFirstResponder];
+}
+
+
+#pragma mark -
+#pragma mark Editing state
 
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated
 {
@@ -135,10 +138,25 @@
     // Hide the back button when editing starts, and show it again when editing finishes.
     [self.navigationItem setHidesBackButton:editing animated:animated];
     
-    [self addPillNoteAddButton];
+    NSUInteger numberOfNonNilProperties = [self calculateNumberOfNonNilProperties];
+    NSArray *pillPropertiesInsertIndexPath = [NSArray arrayWithObject:[NSIndexPath indexPathForRow:numberOfNonNilProperties inSection:NOTES_SECTION]];
+    
+    if (self.editing) { [self setUpUndoManager]; }
+    
+    [self.tableView beginUpdates];
+
+    if (self.editing && numberOfNonNilProperties < 4) {
+        [self.tableView insertRowsAtIndexPaths:pillPropertiesInsertIndexPath withRowAnimation:UITableViewRowAnimationTop];    
+        
+    } else if (numberOfNonNilProperties < 4){
+        [self.tableView deleteRowsAtIndexPaths:pillPropertiesInsertIndexPath withRowAnimation:UITableViewRowAnimationTop];
+    }
+    
+    [self.tableView endUpdates];
     
     if (!self.editing) {
         [self cleanUpUndoManager];
+        
         // Save the changes.
         NSError *error;
         if (![self.pill.managedObjectContext save:&error]) {
@@ -161,7 +179,28 @@
 	return 2;
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSInteger rows = 0;
+    
+    switch (section) {
+        case PILL_SECTION:
+            rows = 2;
+            break;
+        case NOTES_SECTION:
+            rows = [self calculateNumberOfNonNilProperties];
+            if (self.editing && rows < 4) rows++;
+            break;
+		default:
+            break;
+    }
+    return rows;
+}
+
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
     NSString *title = nil;
     
     switch (section) {
@@ -174,49 +213,51 @@
         default:
             break;
     }
-    
     return title;
 }
 
 
-- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
+{
+    NSString *footer = nil;
     
     if (!self.editing && section == NOTES_SECTION) {
-        return @"To add more notes tap the Edit button in the upper right corner.";
-    
-    } else { return @""; }
-}
-
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSInteger rows = 0;
-
-    switch (section) {
-        case PILL_SECTION:
-            rows = 2;
-            break;
-        case NOTES_SECTION:
-            rows = [self calculateNumberOfNonNilProperties];
-            if (self.editing && rows < 4) rows++;
-            //NSLog(@"rows: %d", rows);
-            break;
-		default:
-            break;
+        footer = @"To add more notes tap the Edit button in the upper right corner.";
     }
-    return rows;
+    return footer;
 }
 
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = nil;
-    
-    // For the Ingredients section, if necessary create a new cell and configure it with an additional label for the amount.
-    // Give the cell a different identifier from that used for cells in other sections so that it can be dequeued separately.
-    
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (self.editing) {
+        return indexPath;
+    }
+    return nil;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
     NSUInteger numberOfNonNilProperties = [self calculateNumberOfNonNilProperties];
     
+    if (self.editing && indexPath.section == NOTES_SECTION && indexPath.row < numberOfNonNilProperties) {
+        [self performSegueWithIdentifier:@"EditPillData" sender:self];
+        
+    } else if (self.editing && indexPath.section == NOTES_SECTION) {
+        [self performSegueWithIdentifier:@"AddPillNote" sender:self];
+    }
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    
+    NSUInteger numberOfNonNilProperties = [self calculateNumberOfNonNilProperties];
+    static NSString *PillDetailsCellIdentifier = @"Details Cell";
+    
     if (indexPath.section == PILL_SECTION) {
-        static NSString *PillDetailsCellIdentifier = @"Details Cell";
         cell = [tableView dequeueReusableCellWithIdentifier:PillDetailsCellIdentifier];
         
         if (cell == nil) {
@@ -232,27 +273,22 @@
         } else if (indexPath.row == 1) {
             cell.textLabel.text = @"Amount";
             cell.detailTextLabel.text = [NSString stringWithFormat:@"%d mg", [self.pill.amount integerValue]];
-            
         }
     
     } else if (indexPath.section == NOTES_SECTION) {
         
         if (indexPath.row < numberOfNonNilProperties) {
-            
-            static NSString *PillDetailsCellIdentifier = @"Details Cell";
             cell = [tableView dequeueReusableCellWithIdentifier:PillDetailsCellIdentifier];
             
             if (cell == nil) {
-                // Create a cell to display pill property.
 				cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:PillDetailsCellIdentifier];
-				cell.accessoryType = UITableViewCellAccessoryNone;
 			}
             
             int j=-1, i=0;
-            
-            while (j!=indexPath.row)
-            {
-                if ([[self.pillNotes objectAtIndex:i] length] > 0) { j++; i++;
+            while (j != indexPath.row) {
+                if ([[self.pillNotes objectAtIndex:i] length] > 0) {
+                    j++; i++;
+                
                 } else { i++; }
             }
             i--;
@@ -289,102 +325,42 @@
 			
 			cell = [tableView dequeueReusableCellWithIdentifier:AddPillPropertyCellIdentifier];
 			if (cell == nil) {
-                // Create a cell to display "Add Ingredient".
+                // Create a cell to display "Add note".
 				cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:AddPillPropertyCellIdentifier];
 				cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 			}
             cell.textLabel.text = @"Add note";
-            cell.detailTextLabel.text = @"Choose";
         }
-    } /*else {
-        // If necessary create a new cell and configure it appropriately for the section.  Give the cell a different identifier from that used for cells in the Ingredients section so that it can be dequeued separately.
-        static NSString *MyIdentifier = @"GenericCell";
-        
-        cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
-        if (cell == nil) {
-            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:MyIdentifier] autorelease];
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        }
-        
-        NSString *text = nil;
-        
-        switch (indexPath.section) {
-            case TYPE_SECTION: // type -- should be selectable -> checkbox
-                text = [recipe.type valueForKey:@"name"];
-                cell.accessoryType = UITableViewCellAccessoryNone;
-                cell.editingAccessoryType = UITableViewCellAccessoryDisclosureIndicator;
-                break;
-            case INSTRUCTIONS_SECTION: // instructions
-                text = @"Instructions";
-                cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-                cell.editingAccessoryType = UITableViewCellAccessoryNone;
-                break;
-            default:
-                break;
-        }
-        
-        cell.textLabel.text = text;
-    }*/
-
+    }
     return cell;
-}
-
-
-- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Only allow selection if editing.
-    if (self.editing) {
-        return indexPath;
-    }
-    return nil;
-}
-
-
-/*
- Manage row selection: If a row is selected, create a new editing view controller to edit the property associated with the selected row.
- */
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSUInteger numberOfNonNilProperties = [self calculateNumberOfNonNilProperties];
-    
-    if ((self.editing && indexPath.section == PILL_SECTION) || (self.editing && indexPath.section == NOTES_SECTION && indexPath.row < numberOfNonNilProperties)) {
-        [self performSegueWithIdentifier:@"EditPillData" sender:self];
-    
-    } else if (self.editing && indexPath.section == NOTES_SECTION) {
-        [self performSegueWithIdentifier:@"AddPillNote" sender:self];
-    }
 }
 
 
 #pragma mark -
 #pragma mark Editing rows
 
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
 	UITableViewCellEditingStyle style = UITableViewCellEditingStyleNone;
     NSUInteger numberOfNonNilProperties = [self calculateNumberOfNonNilProperties];
-    
-    // Only allow editing in the ingredients section.
-    // In the ingredients section, the last row (row number equal to the count of ingredients) is added automatically (see tableView:cellForRowAtIndexPath:) to provide an insertion cell, so configure that cell for insertion; the other cells are configured for deletion.
+ 
     if (indexPath.section == NOTES_SECTION) {
         // If this is the last item, it's the insertion row.
         if (indexPath.row == numberOfNonNilProperties) {
             style = UITableViewCellEditingStyleInsert;
         
-        }else {
+        } else {
             style = UITableViewCellEditingStyleDelete;
         }
-        
     }
-    
     return style;
 }
 
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath 
+{    
     // Only allow deletion, and only in the NOTES_SECTION
     if ((editingStyle == UITableViewCellEditingStyleDelete) && (indexPath.section == NOTES_SECTION)) {
-        
          UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
         
         // Set the action name for the undo operation.
@@ -420,12 +396,12 @@
         [self.tableView beginUpdates];
         [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationTop];
         
-        if (numberOfNonNilProperties < 4 && ([self.tableView numberOfRowsInSection:NOTES_SECTION]-1) == numberOfNonNilProperties)
-        {
+        if (numberOfNonNilProperties < 4 && ([self.tableView numberOfRowsInSection:NOTES_SECTION]-1) == numberOfNonNilProperties) {
             [self.tableView insertRowsAtIndexPaths:pillPropertiesInsertIndexPath withRowAnimation:UITableViewRowAnimationTop];
         }
         [self.tableView endUpdates];
         
+        // Saving deletion and possible insertion changes
         NSError *error;
         if (![self.pill.managedObjectContext save:&error])
         {
@@ -441,20 +417,16 @@
 
 - (void)setUpUndoManager
 {
-    /*
-     If the book's managed object context doesn't already have an undo manager, then create one and set it for the context and self.
-     The view controller needs to keep a reference to the undo manager it creates so that it can determine whether to remove the undo manager when editing finishes.
-     */
+    //If the pill's managed object context doesn't already have an undo manager, then create one and set it for the context and self.
+    //The view controller needs to keep a reference to the undo manager it creates so that it can determine whether to remove the undo manager when editing finishes.
     if (self.pill.managedObjectContext.undoManager == nil) {
         
         NSUndoManager *anUndoManager = [[NSUndoManager alloc] init];
         [anUndoManager setLevelsOfUndo:3];
         self.undoManager = anUndoManager;
-        
         self.pill.managedObjectContext.undoManager = self.undoManager;
     }
-    
-    // Register as an observer of the book's context's undo manager.
+    // Register as an observer of the pill's context's undo manager.
     NSUndoManager *pillUndoManager = self.pill.managedObjectContext.undoManager;
     
     NSNotificationCenter *dnc = [NSNotificationCenter defaultCenter];
@@ -481,14 +453,14 @@
 }
 
 
-- (void)undoManagerDidUndo:(NSNotification *)notification {
-    // Redisplay the data.    
+- (void)undoManagerDidUndo:(NSNotification *)notification
+{
     [self updateRightBarButtonItemState];
 }
 
 
-- (void)undoManagerDidRedo:(NSNotification *)notification {
-    // Redisplay the data.    
+- (void)undoManagerDidRedo:(NSNotification *)notification
+{
     [self updateRightBarButtonItemState];
 }
 
@@ -502,19 +474,8 @@
 }
 
 
-- (void)viewDidAppear:(BOOL)animated
-{
-    [super viewDidAppear:animated];
-    [self becomeFirstResponder];
-}
-
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-    [super viewWillDisappear:animated];
-    [self resignFirstResponder];
-}
-
+#pragma mark -
+#pragma mark Segue management
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
